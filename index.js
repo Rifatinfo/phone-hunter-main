90be02f0949f214193aed0ca6bdac578c745c98e
Rifatinfo <mdrifathossainsinfo@gmail.com>
2024-12-18 17:18:05 +0600

    handle show more button

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 
-const loadAllPhones = async () =>{
+const loadAllPhones = async (status) =>{
     document.getElementById('spinner').style.display = 'none';
 
     const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=iphone`);
     const data = await response.json();
-    console.log(data.data);
+     {status ? displayAllPhone(data.data) :  displayAllPhone(data.data.slice(0,6))};
 }
 
-const displayData = async (phone) => {
+const displayAllPhone = async (phone) => {
     console.log(phone);
+}
+
+const handleShowAll = () =>{
+    loadAllPhones(true);
 }
 
 const handleSearch = () => {
